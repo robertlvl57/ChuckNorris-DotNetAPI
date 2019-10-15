136bff97784b423f5d7771c90522813ae71a94a8
robert lowry <lowryrob2001@gmail.com>
2019-10-14 17:00:06 -0700

    Added a button to get jokes and a list of categories

---
+++ ChuckNorrisJokeGenerator/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ChuckNorrisJokeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetJoke = new System.Windows.Forms.Button();
+            this.cmbCategories = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnGetJoke
+            // 
+            this.btnGetJoke.Location = new System.Drawing.Point(96, 66);
+            this.btnGetJoke.Name = "btnGetJoke";
+            this.btnGetJoke.Size = new System.Drawing.Size(75, 23);
+            this.btnGetJoke.TabIndex = 0;
+            this.btnGetJoke.Text = "Get Joke";
+            this.btnGetJoke.UseVisualStyleBackColor = true;
+            this.btnGetJoke.Click += new System.EventHandler(this.btnGetJoke_Click);
+            // 
+            // cmbCategories
+            // 
+            this.cmbCategories.FormattingEnabled = true;
+            this.cmbCategories.Location = new System.Drawing.Point(213, 66);
+            this.cmbCategories.Name = "cmbCategories";
+            this.cmbCategories.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategories.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 362);
+            this.Controls.Add(this.cmbCategories);
+            this.Controls.Add(this.btnGetJoke);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetJoke;
+        private System.Windows.Forms.ComboBox cmbCategories;
+    }
+}
+
---
+++ ChuckNorrisJokeGenerator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChuckNorrisAPI;
+
+namespace ChuckNorrisJokeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetJoke_Click(object sender, EventArgs e)
+        {
+            Joke j = await ChuckNorrisClient.GetRandomJoke();
+            MessageBox.Show(j.JokeText);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateComboBox();
+        }
+
+        public async void PopulateComboBox()
+        {
+            var c = await ChuckNorrisClient.GetCategories();
+            foreach(var categories in c)
+            {
+                cmbCategories.Items.Add(categories);
+            }
+        }
+    }
+}
